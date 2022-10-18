c98b7afbd625b1c2338b23c4ce2afb3f393ea5fd
Jorge Luiz Figueira <jorgeluizfigueira@gmail.com>
2022-10-18 17:59:18 -0300

    Upload projeto modulo-2 e devolutiva modulo-1

---
+++ modulo-2/README.md
@@ -0,0 +1,45 @@
+# <p align="center"> <b> Jogo da Forca </b> 
+# <p align="center"> <b> Projeto Lógica de programação II  </b> 
+
+Nesse projeto iremos focar no conhecimento adquirido durante os módulos de lógica de programação I e II.
+
+Utilizaremos as estruturas de dados (tuplas, listas, dicionários) além da lógica de programação (if/else, while, e for), lembre-se das compreensões de listas e dicionários. E utilize as técnicas e algoritmos que achar necessário para realizar o projeto
+
+## Dados
+Será utilizado os dados (dataset) de frutas (frutas.txt) disponível em um arquivo separado.
+
+Nesse arquivo, cada linha representa uma palavra correspondente a uma fruta distinta.
+
+## O problema
+Nesse projeto você deverá desenvolver um programa que permita aceitar ações da pessoa usuária (input).
+
+Nesse processo, o programa deve selecionar uma palavra aleatória do arquivo frutas.txt.
+
+A partir da palavra aleatória, crie um jogo da forca, em que a pessoa usuária pode inserir um caracter, e se tiver acerto, a palavra será preenchida. Em caso de erro, será retirada uma "vida" da pessoa usuária e o desenho da pessoa na forca deve ser preenchida.
+
+Na tela, teremos:
+
+Desenho da forca
+Letras já escolhidas
+Uma linha mostrando a palavra.
+No inicio cada letra da palavra a ser adivinhada é representada com _
+Ao acertar a letra presente na palavra, as posições referentes a essa letra serão substituidas e mostrada para a pessoa usuária.
+Para auxiliar no desenvolvimento, podemos dividir o projeto nas seguintes fases:
+
+Escolha da palavra aleatória
+Representar essa palavra com _
+Ao acertar uma letra substituir o _ pela letra nas posições corretas
+Ao chegar no máximo de erros, encerrar o jogo
+Ao acetar a palavra, finalizar o jogo
+Ao errar uma letra, apresentar essas para a pessoa usuária
+Ao errar uma letra, modificar o desenho da forca
+Critérios de avaliação
+
+## Os seguintes itens serão avaliados:
+
+- Reprodutibilidade do código: seu código será executado e precisa gerar os mesmos resultados apresentados por você;
+
+- Clareza: seu código precisa ser claro e deve existir uma linha de raciocínio direta. Comente o código em pontos que julgar necessário para o entendimento total;
+
+- Utilize funções para melhorar a clareza do código!
+
---
+++ modulo-2/forca.py
@@ -0,0 +1,182 @@
+import os
+from glob import glob
+from random import choice
+from time import sleep
+
+
+erro_0 = """
+   |-------
+   |      |
+   |    
+   |    
+   |    
+   |     
+   |     
+___|___ 
+"""
+erro_1 = """
+   |-------
+   |      |
+   |      _
+   |     |_|
+   |      
+   |     
+   |     
+___|___ 
+"""
+
+erro_2 = ("""
+   |-------
+   |      |
+   |      _
+   |     |_|
+   |      |
+   |      |
+   |     
+___|___ 
+""")
+
+erro_3 = ("""
+   |-------
+   |      |
+   |      _
+   |     |_|
+   |    --|
+   |      |
+   |     
+___|___ 
+""")
+
+erro_4 = ("""
+   |-------
+   |      |
+   |      _
+   |     |_|
+   |    --|--
+   |      |
+   |     
+___|___ 
+""")
+
+erro_5 = ("""
+   |-------
+   |      |
+   |      _
+   |     |_|
+   |    --|--
+   |      |
+   |     / 
+___|___ 
+""")
+
+erro_6 = ("""
+   |-------
+   |      |
+   |      _
+   |     |_|
+   |    --|--
+   |      |
+   |     / \\
+___|___ 
+""")
+
+grafico_forca = [erro_0,erro_1,erro_2,erro_3,erro_4,erro_5,erro_6]
+
+
+def limpa_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def sorteia_palavra():
+  # cria uma lista com todos os paths de arquivos TXT
+  listas_palavras = glob('*.txt')
+  if listas_palavras:
+    # sorteia um path desses arquivos
+    lista_sorteada = choice(listas_palavras)
+    # carrega o arquivo e cria uma lista das palavras
+    with open(lista_sorteada) as arquivo:
+      palavras = arquivo.read().split('\n')
+    palavras = [palavra.upper() for palavra in palavras if palavra != '']
+    # se houver palavras, sorteia uma
+    if palavras:
+      palavra_sorteada = choice(palavras)
+      classe_palavra = lista_sorteada.replace('.txt','').upper()
+    
+      return classe_palavra, palavra_sorteada
+    else:
+      return (None,None)
+  else:
+    return (None,None)
+
+
+def palavra_secreta(palavra:str,mostrar_letras=[]):
+  palavra_secreta = ''.join([letra if letra in mostrar_letras else ' _ ' for letra in palavra])
+  return palavra_secreta
+
+
+def dica(classe_palavra:str, palavra_sorteada:str):
+  print(f'Pertence a {classe_palavra} com {len(palavra_sorteada)} letras.')
+
+
+def leia_letra(msg):
+    letra = input(msg)
+    while not (len(letra) == 1 and letra.isalpha()):
+        print('Entrada inválida!')
+        letra = input(msg)
+    return letra.upper()
+
+
+def leia_resposta(msg:str) -> bool:
+    resposta = input(msg).lower() 
+    while resposta not in ('s','n'): 
+        print('Resposta Inválida') 
+        resposta = input(msg).lower() 
+    if resposta == 's': 
+        return True 
+    else: 
+        return False
+
+def main():
+    while True:
+        classe_palavra, palavra_sorteada = sorteia_palavra()
+        if not classe_palavra:
+            print('Arquivo de palavras ausente!')
+            break
+        else:
+            erros = 0
+            letras_escolhidas = []
+            while erros < 6:
+                print('{:=^20}'.format('Jogo da Forca'))
+                dica(classe_palavra, palavra_sorteada)
+                if letras_escolhidas:
+                    print(f'Letras já escolhidas:{" ".join(letras_escolhidas)}')
+                print(grafico_forca[erros])
+                print(palavra_secreta(palavra_sorteada,letras_escolhidas))
+                letra_usuario = leia_letra('Digite uma letra: ')
+            
+                if letra_usuario not in letras_escolhidas:
+                    letras_escolhidas.append(letra_usuario)
+
+                    if letra_usuario not in palavra_sorteada:
+                        erros += 1
+                        print(f'Letra {letra_usuario} não faz parte da palavra.')
+                        sleep(2)
+
+                    if palavra_secreta(palavra_sorteada,letras_escolhidas) == palavra_sorteada:
+                        print(f'Parabéns, você acertou! A palavra é {palavra_sorteada}.')
+                        break
+                else:
+                    print(f'Você já escolheu a letra {letra_usuario}!')    
+                    sleep(2)
+                limpa_tela()
+                
+            if erros == 6:
+                print(f'Que pena! Você perdeu. A palavra era : {palavra_sorteada}')
+                
+            continuar = leia_resposta('Deseja continuar (s/n)? ')
+            limpa_tela()
+            if not continuar:
+                break
+
+
+main()
